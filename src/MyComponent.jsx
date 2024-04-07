ade2b27f4448058c2f5fd5d141dcd35595956829
Induwara Thisaru <induwarathisaru2002@gmail.com>
2024-04-07 21:51:58 +0530

    i stopped at useEffect hook

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,57 +1,24 @@
 
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 function MyComponent(){
 
-    const [cars, setCars] = useState([]);
-    const [carYear, setCarYear] = useState(new Date().getFullYear());
-    const [carMake, setCarMake] = useState("");
-    const [carModel, setCarModel] = useState("");
+    const [count, setCount] = useState(0);
 
-    function handleAddCar(){
-        const newCar = {year: carYear,
-                    make: carMake,
-                    model: carModel}
+    useEffect(
+        () => {
+            document.title = `Count: ${count}`
+        }
+    )
 
-        setCars(prevCars => [...prevCars, newCar])
-
-        setCarYear(new Date().getFullYear());
-        setCarMake(" ");
-        setCarModel(" ");
-    }
-
-    function handleRemoveCar(index){
-        setCars(prevCars => prevCars.filter((_, i) => i !== index))
-    }
-
-    function handleYearChange(event){
-        setCarYear(event.target.value)
-    }
-
-    function handleMakeChange(event){
-        setCarMake(event.target.value)
-    }
-
-    function handleModelChange(event){
-        setCarModel(event.target.value)
+    const addCount = () => {
+        setCount(c => c+1)
     }
 
     return(
         <div>
-            <h2>List of Car Objects</h2>
-
-            <ul>
-                {cars.map((car, index) => 
-                        <li key={index} onClick={() => handleRemoveCar(index)}>
-                            {car.year} {car.make} {car.model}
-                        </li>
-                )}
-            </ul>
-
-            <input type="number" value={carYear} onChange={handleYearChange}/><br />
-            <input type="text" value={carMake} onChange={handleMakeChange} placeholder="enter car make"/><br />
-            <input type="text" value={carModel} onChange={handleModelChange} placeholder="Enter car model"/><br />
-            <button onClick={handleAddCar}>Add Car</button>
+            <p>Count: {count}</p>
+            <button onClick={addCount}>Add</button>
         </div>
     )
 }
